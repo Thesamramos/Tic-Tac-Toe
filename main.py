5327bc7f546c5ac829c0efb999d039807648b460
Samuel Ramos <104317445+Thesamramos@users.noreply.github.com>
2024-12-11 18:04:17 -0400

    Função checkWin criada para checar as condições de vitoria

--- main.py
+++ main.py
@@ -7,3 +7,14 @@ def printBoard(board):
     print(f"{board[3]} | {board[4] | {board[5]}}")
     print("---------")
     print(f"{board[6]} | {board[7] | {board[8]}}")
+
+def checkWin(board, player):
+    win_patterns = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8],
+        [0, 3, 6], [1, 4, 7], [2, 5, 8],
+        [0, 4, 8], [2, 4, 6]
+        ]
+    for pattern in win_patterns:
+        if all(board[i] == player for i in pattern):
+            return True
+    return False
